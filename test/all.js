b2e1eaaf66e3a5ff4dd486a4936ebe16e11afb66
sharkbait-au <sharkbait.aust@gmail.com>
2015-05-03 17:59:34 +1000

    auto format

--- test/all.js
+++ test/all.js
@@ -1,15 +1,11 @@
-var assert = require('assert'),
-	nodeModuleDemo = require('../index'),
-	escape = nodeModuleDemo.escape,
-	unescape = nodeModuleDemo.unescape;
-	
+var assert = require('assert'), nodeModuleDemo = require('../index'), escape = nodeModuleDemo.escape, unescape = nodeModuleDemo.unescape;
 
 describe('#escape', function() {
-  it('converts & into &amp;', function() {
-    assert.strictEqual(escape('&'),'&amp;','&amp;');
-  });
+	it('converts & into &amp;', function() {
+		assert.strictEqual(escape('&'), '&amp;', '&amp;');
+	});
 
-  it('converts " into &quot;', function() {
-	  assert.strictEqual(escape('"'),'&quot;','&quot;');
-  });
+	it('converts " into &quot;', function() {
+		assert.strictEqual(escape('"'), '&quot;', '&quot;');
+	});
 });
