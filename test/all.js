0f61d0462d16043967e1e829e5bdf8d552cbce55
sharkbait-au <sharkbait.aust@gmail.com>
2015-05-03 17:55:34 +1000

    some code & mocha testing

---
+++ test/all.js
@@ -0,0 +1,15 @@
+var	assert = require('assert'),
+	nodeModuleDemo = require('../index'),
+    escape = nodeModuleDemo.escape,
+    unescape = nodeModuleDemo.unescape;
+	
+
+describe('#escape', function() {
+  it('converts & into &amp;', function() {
+    assert.strictEqual(escape('&'),'&amp;','&amp;');
+  });
+
+  it('converts " into &quot;', function() {
+	  assert.strictEqual(escape('"'),'&quot;','&quot;');
+  });
+});
